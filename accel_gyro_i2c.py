3fc66982e2b87a9fbd6c7c6e485eefd157d95aa7
Sebastian Monzon <smonzon360@gmail.com>
2020-07-07 18:28:56 -0400

    Added Accelerometer/ Gyro iterface for MPU module

--- accel_gyro_i2c.py
+++ accel_gyro_i2c.py
@@ -48,6 +48,22 @@ def read_raw_data(addr):
     value = value - 65536
   return value
 
+OFFSETS = [0.3, -0.11, -0.22, 0, 0, 0.11]
+
+
+def get_acc_x():
+  return read_raw_data(ACCEL_XOUT_H)/16384.0 + OFFSETS[0]
+def get_acc_y():
+  return read_raw_data(ACCEL_YOUT_H)/16384.0 + OFFSETS[1]
+def get_acc_z():
+  return read_raw_data(ACCEL_ZOUT_H)/16384.0 + OFFSETS[2]
+
+def get_gyro_x():
+  return read_raw_data(GYRO_XOUT_H)/131.0 + OFFSETS[3]
+def get_gyro_y():
+  return read_raw_data(GYRO_YOUT_H)/131.0 + OFFSETS[4]
+def get_gyro_z():
+  return read_raw_data(GYRO_ZOUT_H)/131.0 + OFFSETS[5]
 
 bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x68   # MPU6050 device address
@@ -58,24 +74,13 @@ print (" Reading Data of Gyroscope and Accelerometer")
 
 while True:
 
-  #Read Accelerometer raw value
-  acc_x = read_raw_data(ACCEL_XOUT_H)
-  acc_y = read_raw_data(ACCEL_YOUT_H)
-  acc_z = read_raw_data(ACCEL_ZOUT_H)
+  Ax = get_acc_x()
+  Ay = get_acc_y()
+  Az = get_acc_z()
 
-  #Read Gyroscope raw value
-  gyro_x = read_raw_data(GYRO_XOUT_H)
-  gyro_y = read_raw_data(GYRO_YOUT_H)
-  gyro_z = read_raw_data(GYRO_ZOUT_H)
-
-  #Full scale range +/- 250 degree/C as per sensitivity scale factor
-  Ax = acc_x/16384.0
-  Ay = acc_y/16384.0
-  Az = acc_z/16384.0
-
-  Gx = gyro_x/131.0
-  Gy = gyro_y/131.0
-  Gz = gyro_z/131.0
+  Gx = get_gyro_x()
+  Gy = get_gyro_y()
+  Gz = get_gyro_z()
 
   print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)
   sleep(0.2)
